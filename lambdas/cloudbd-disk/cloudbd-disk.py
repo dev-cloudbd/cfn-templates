4ee3c874e6334fedc59f57e13020310faa84e6bf
Shaun McDowell <smcdowell@cloudbd.io>
2019-08-23 11:32:40 -0400

    provide more detailed status messages on custom resource lambda errors

--- lambdas/cloudbd-disk/cloudbd-disk.py
+++ lambdas/cloudbd-disk/cloudbd-disk.py
@@ -19,12 +19,17 @@ EXIT_SUCCESS = 0
 EXIT_FAILURE = 1
 EXIT_DISK_NOT_FOUND = 2
 
-def send(event, context, responseStatus, responseData=None, physicalResourceId=None, noEcho=False):
+def send(event, context, responseStatus, responseData=None, physicalResourceId=None, noEcho=False, responseReason=None):
   responseUrl = event['ResponseURL']
+  if responseReason:
+    responseReason += ' '
+  else:
+    responseReason = ''
+  responseReason += 'See the details in CloudWatch Log Stream: ' + context.log_stream_name
 
   responseBody = {}
   responseBody['Status'] = responseStatus
-  responseBody['Reason'] = 'See the details in CloudWatch Log Stream: ' + context.log_stream_name
+  responseBody['Reason'] = responseReason
   responseBody['PhysicalResourceId'] = physicalResourceId or context.log_stream_name
   responseBody['StackId'] = event['StackId']
   responseBody['RequestId'] = event['RequestId']
@@ -50,6 +55,9 @@ def send(event, context, responseStatus, responseData=None, physicalResourceId=N
 class TimeoutError(Exception):
   pass
 
+class CredentialsError(Exception):
+  pass
+
 def timeout_handler(_signal, _frame):
   raise TimeoutError('Time limit exceeded')
 
@@ -57,11 +65,15 @@ signal.signal(signal.SIGALRM, timeout_handler)
 creds_path = "/tmp/cloudbd-credentials.json"
 
 def get_credentials(ssm_region, ssm_parameter):
-  if not os.path.isfile(creds_path):
-    ssm = boto3.client('ssm', region_name=ssm_region)
-    creds = ssm.get_parameter(Name=ssm_parameter, WithDecryption=True)['Parameter']['Value']
-    with open(creds_path, "w") as creds_file:
-      creds_file.write(creds)
+  try:
+    if not os.path.isfile(creds_path):
+      ssm = boto3.client('ssm', region_name=ssm_region)
+      creds = ssm.get_parameter(Name=ssm_parameter, WithDecryption=True)['Parameter']['Value']
+      with open(creds_path, "w") as creds_file:
+        creds_file.write(creds)
+  except Exception as e:
+    LOGGER.info(str(e))
+    raise CredentialsError("unable to download CloudBD credentials from SSM paramater store")
 
 def create_disk(remote, disk, size):
   subprocess.check_call(["cloudbd", "create", "--creds=" + creds_path, "--remote=" + remote, "--disk=" + disk, "--size=" + size])
@@ -80,14 +92,16 @@ def handler(event, context):
 
   try:
     requestType = event['RequestType']
+
     if requestType == 'Update':
       LOGGER.info("Request failed: cannot update a CloudBD disk")
-      physicalResourceId = event['PhysicalResourceId']
-      send(event, context, FAILED, None, physicalResourceId)
+      send(event, context, FAILED, None, physicalResourceId, None, "Cannot change CloudBD disk properties")
       return
-
-    disk = event['ResourceProperties']['Name']
-    size = event['ResourceProperties']['Size']
+    elif requestType == 'Delete':
+      physicalResourceId = event['PhysicalResourceId']
+      if physicalResourceId == "0":
+        send(event, context, SUCCESS, None, physicalResourceId)
+        return
 
     get_credentials(os.environ['CLOUDBD_CREDS_SSM_REGION'], os.environ['CLOUDBD_CREDS_SSM_PARAM'])
 
@@ -100,10 +114,11 @@ def handler(event, context):
       "secret_access_key": os.environ['AWS_SECRET_ACCESS_KEY'],
       "session_token": os.environ['AWS_SESSION_TOKEN']
     }
-
     remote = 'data:application/json,' + json.dumps(remote_dict, separators=(",",":"))
+    disk = event['ResourceProperties']['Name']
 
     if requestType == 'Create':
+      size = event['ResourceProperties']['Size']
       LOGGER.info("Request cloudbd create disk '%s' of size '%s'", disk, size)
       create_disk(remote, disk, size)
       diskinfo = get_disk_info(remote, disk)
@@ -118,29 +133,29 @@ def handler(event, context):
     elif requestType == 'Delete':
       physicalResourceId = event['PhysicalResourceId']
       LOGGER.info("Request cloudbd destroy disk '%s (%s)'", disk, physicalResourceId)
-      if physicalResourceId == "0":
+      try:
+        diskinfo = get_disk_info(remote, disk)
+        if "cbd-" + diskinfo['CBD_UUID'] == physicalResourceId:
+          delete_disk(remote, disk)
+        else:
+          LOGGER.info("Disk UUID mismatch, non-CloudFormation managed resource exists... skipping delete")
         send(event, context, SUCCESS, None, physicalResourceId)
-      else:
-        try:
-          diskinfo = get_disk_info(remote, disk)
-          if "cbd-" + diskinfo['CBD_UUID'] == physicalResourceId:
-            delete_disk(remote, disk)
-          else:
-            LOGGER.info("Disk UUID mismatch, non-CloudFormation managed resource exists... skipping delete")
+      except subprocess.CalledProcessError as e:
+        if e.returncode == EXIT_DISK_NOT_FOUND:
+          LOGGER.info("Disk not found, assuming already deleted")
           send(event, context, SUCCESS, None, physicalResourceId)
-        except subprocess.CalledProcessError as e:
-          if e.returncode == EXIT_DISK_NOT_FOUND:
-            LOGGER.info("Disk not found, assuming already deleted")
-            send(event, context, SUCCESS, None, physicalResourceId)
-          else:
-            LOGGER.info("Failed to delete disk '%s': %s", disk, e.output)
-            send(event, context, FAILED, None, physicalResourceId)
+        else:
+          LOGGER.info("Failed to delete disk '%s': %s", disk, e.output)
+          send(event, context, FAILED, None, physicalResourceId)
     else:
       LOGGER.info("Request failed: unexpected event type '%s'", event['RequestType'])
       send(event, context, FAILED, None, physicalResourceId)
   except TimeoutError:
     LOGGER.info("Request failed: time limit exceeded")
-    send(event, context, FAILED, None, physicalResourceId)
+    send(event, context, FAILED, None, physicalResourceId, None, "Time limit exceeded.")
+  except CredentialsError:
+    LOGGER.info("Request failed: unable to get CloudBD credentials from SSM parameter")
+    send(event, context, FAILED, None, physicalResourceId, None, "Failed to get CloudBD credentials from SSM parameter.")
   except Exception as e:
     LOGGER.info("Request failed: %s", repr(e))
     send(event, context, FAILED, None, physicalResourceId)
